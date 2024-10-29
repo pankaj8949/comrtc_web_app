847e0da8ae9abcfc6a0f13617a2b05bc44b43091
pankaj8949 <desktubegaming@gmail.com>
2024-10-30 01:09:17 +0530

    Complete template rendering and home page is looking alike

--- main.py
+++ main.py
@@ -6,5 +6,57 @@ app = Flask(__name__)
 def Home():
     return render_template('index.html')
 
+@app.route("/organizing-committee")
+def OrganizingCommittee():
+    return render_template('screens/about/oc.html')
+
+@app.route("/scientific-committee")
+def ScientificCommittee():
+    return render_template('screens/about/sc.html')
+
+@app.route("/scientific-committee-lead")
+def ScientificCommitteeLead():
+    return render_template('screens/about/scl.html')
+
+@app.route("/join-the-scientific-committee")
+def JoinTheScientificCommittee():
+    return render_template('screens/about/jtsc.html')
+
+@app.route("/service")
+def Service():
+    return render_template('service.html')
+
+@app.route("/important-dates")
+def ImportantDates():
+    return render_template('screens/Guide for authors/importantdates.html')
+
+@app.route("/review-process")
+def ReviewProcess():
+    return render_template('screens/Guide for authors/reviewprocess.html')
+
+@app.route("/submission-guidelines")
+def SubmissionGuidelines():
+    return render_template('screens/Guide for authors/subgl.html')
+
+@app.route("/submission")
+def Submission():
+    return render_template('screens/Guide for authors/submission.html')
+
+@app.route("/paper-status")
+def PaperStatus():
+    return render_template('screens/Guide for authors/paperstatus.html')
+
+@app.route("/detailed-schedule")
+def DetailedSchedule():
+    return render_template('screens/program/detailedschedule.html')
+
+@app.route("/awards-grants")
+def AwardsAndGrants():
+    return render_template('screens/program/awardsngrants.html')
+
+@app.route("/contact")
+def Contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
